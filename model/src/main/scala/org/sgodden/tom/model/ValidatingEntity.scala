f3abc0a85059b443319e681815df063c3a71e65b
Simon Godden <simon@blackcatsolutions.co.uk>
2013-05-17 07:41:05 +0100

    Introduce concept of validating entity and work it in to the base repository

---
+++ model/src/main/scala/org/sgodden/tom/model/ValidatingEntity.scala
@@ -0,0 +1,12 @@
+package org.sgodden.tom.model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: simon
+ * Date: 17/05/2013
+ * Time: 07:26
+ * To change this template use File | Settings | File Templates.
+ */
+trait ValidatingEntity {
+
+}
